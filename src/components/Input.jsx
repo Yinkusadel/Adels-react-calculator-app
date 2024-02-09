3392b6a7a272b8e91f0f605a9cf02b71655d8850
yinkusadel <adeleyeolayinka73@gmail.com>
2024-02-09 21:47:21 +0100

    refactor: input and label to mk code dry

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,10 +4,49 @@ const Input = ({
   value,
   checked,
   style = 'duration-200 h-4 w-4 cursor-pointer appearance-none rounded-full transition-all',
+  className,
 }) => {
   return (
-    <input className={style} id={id} name={name} type="radio" value={value} checked={checked} />
+    <input
+      className={`${style} ${className}`}
+      id={id}
+      name={name}
+      type="radio"
+      value={value}
+      checked={checked}
+    />
   );
 };
 
-export default Input;
+const inputProps = [
+  {
+    className: "themeOne checked checked:bg-themeOne-mobile-radio-input-checked checked:hover:bg-themeOne-mobile-radio-input-checked-hover",
+    id: "theme-choice-one",
+    value: "themeOne",
+    checked: true
+  },
+  {
+    className: "themeTwo checked:bg-themeTwo-mobile-radio-input-checked checked:hover:bg-themeTwo-mobile-radio-input-checked-hover",
+    id: "theme-choice-two",
+    value: "themeTwo",
+  },
+  {
+    className: "themeThree checked:bg-themeThree-mobile-radio-input-checked checked:hover:bg-themeThree-mobile-radio-input-checked-hover",
+    id: "theme-choice-three",
+    value: "themeThree",
+  }
+];
+
+const ThemeInputs = () => {
+  return (
+    <>
+          {inputProps.map((props, index) => (
+        <Input key={index} {...props} />
+      ))}
+    </>
+
+   
+  );
+};
+
+export default ThemeInputs;
--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -1,3 +1,9 @@
+const themes = [
+  { id: 'theme-choice-one', value: '1' },
+  { id: 'theme-choice-two', value: '2' },
+  { id: 'theme-choice-three', value: '3' },
+];
+
 const Label = ({ value, htmlFor }) => {
   return (
     <label className="flex w-4 justify-center" htmlFor={htmlFor}>
@@ -6,4 +12,14 @@ const Label = ({ value, htmlFor }) => {
   );
 };
 
-export default Label;
+const LabelSelector = () => {
+  return (
+    <>
+      {themes.map((theme) => (
+        <Label key={theme.id} value={theme.value} htmlFor={theme.id} />
+      ))}
+    </>
+  );
+};
+
+export default LabelSelector;
