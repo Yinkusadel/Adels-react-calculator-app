e1094c5123d077d701b86bf66004ace0abbd710b
yinkusadel <adeleyeolayinka73@gmail.com>
2024-02-10 19:25:58 +0100

    refactor: button to be mapped and rendered

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,49 @@
-const Button = ({
-  value,
-  style = 'h-12 w-1/5 rounded bg-themeOne-80 text-3xl text-themeOne-100 shadow-ash hover:bg-themeOne-90 active:translate-y-1',
-}) => {
+const Button = ({ value }) => {
+  let style =
+    'h-12 w-1/5 rounded bg-themeOne-80 text-3xl text-themeOne-100 shadow-ash hover:bg-themeOne-90 active:translate-y-1';
+
+  switch (value) {
+    case 'DEL':
+      style =
+        'delete-btn h-12 w-1/5 rounded bg-themeOne-120 text-2xl font-bold text-themeOne-white shadow-darkblue hover:bg-themeOne-130 active:translate-y-1';
+      break;
+    case 'RESET':
+      style =
+        ' h-12 w-[48%] rounded bg-themeOne-150 text-2xl font-bold text-themeOne-white shadow-darkblue hover:bg-themeOne-160 active:translate-y-1';
+      break;
+    case '=':
+      style =
+        ' h-12 w-[48%] rounded bg-themeOne-180 text-2xl font-bold text-themeOne-white shadow-red hover:bg-themeOne-190 active:translate-y-1';
+      break;
+    default:
+      break;
+  }
+
   return (
-    <>
-      <button className={style} type="button" value={value}>
-        {value}
-      </button>
-    </>
+    <button className={style} type="button" value={value}>
+      {value}
+    </button>
   );
 };
 
-export default Button;
+const CalcButtons = () => {
+  const ButtonValues = [
+    ['7', '8', '9', 'DEL'],
+    ['4', '5', '6', '+'],
+    ['1', '2', '3', '-'],
+    ['.', '0', '/', 'x'],
+    ['RESET', '='],
+  ];
+
+  const MapButton = ButtonValues.map((row) => (
+    <div className="mb-2 flex items-center justify-between">
+      {row.map((buttonValue) => (
+        <Button value={buttonValue} />
+      ))}
+    </div>
+  ));
+
+  return <>{MapButton}</>;
+};
+
+export default CalcButtons;
--- src/components/ButtonContainer.jsx
+++ src/components/ButtonContainer.jsx
@@ -1,46 +1,10 @@
-import Button from './Button';
+import CalcButtons from './Button';
 
 const ButtonContainer = () => {
   return (
     <div class=" h-[65vh]">
       <div class=" mt-5 w-[300px] rounded-lg bg-themeOne-70 p-5 ">
-        <div class="  mb-2 flex items-center justify-between">
-          <Button value="7" />
-          <Button value="8" />
-          <Button value="9" />
-          <Button
-            value="DEL"
-            style="delete-btn h-12 w-1/5 rounded bg-themeOne-120 text-2xl font-bold text-themeOne-white shadow-darkblue hover:bg-themeOne-130 active:translate-y-1"
-          />
-        </div>
-        <div class="  mb-2 flex items-center justify-between">
-          <Button value="4" />
-          <Button value="5" />
-          <Button value="6" />
-          <Button value="+" />
-        </div>
-        <div class="  mb-2 flex items-center justify-between">
-          <Button value="1" />
-          <Button value="2" />
-          <Button value="3" />
-          <Button value="-" />
-        </div>
-        <div class="  mb-2 flex items-center justify-between">
-          <Button value="." />
-          <Button value="0" />
-          <Button value="/" />
-          <Button value="x" />
-        </div>
-        <div class="  mb-2 flex items-center justify-between">
-          <Button
-            value="RESET"
-            style=" h-12 w-[48%] rounded bg-themeOne-150 text-2xl font-bold text-themeOne-white shadow-darkblue hover:bg-themeOne-160 active:translate-y-1"
-          />
-          <Button
-            value="="
-            style=" h-12 w-[48%] rounded bg-themeOne-180 text-2xl font-bold text-themeOne-white shadow-red hover:bg-themeOne-190 active:translate-y-1"
-          />
-        </div>
+        <CalcButtons />
       </div>
     </div>
   );
