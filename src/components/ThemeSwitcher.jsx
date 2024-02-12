46b202a506bc768b64ae1cdef00b1d862ee1dd9f
yinkusadel <adeleyeolayinka73@gmail.com>
2024-02-12 13:04:52 +0100

    refactor: theme switcher

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,28 +1,23 @@
 import Label from './Label';
 import Input from './Input';
 
-const Themes = [
-  { id: 'theme-choice-one', value: '1' },
-  { id: 'theme-choice-two', value: '2' },
-  { id: 'theme-choice-three', value: '3' },
-];
-
-const inputProps = [
+const labelInput = [
   {
-    className: 'checked checked:bg-themeOne-30 checked:hover:bg-themeOne-40',
     id: 'theme-choice-one',
-    value: 'themeOne',
+    value: '1',
+    className: 'checked checked:bg-themeOne-30 checked:hover:bg-themeOne-40',
+
     checked: true,
   },
   {
-    className: 'checked:bg-themeTwo-30 checked:hover:bg-themeTwo-40',
     id: 'theme-choice-two',
-    value: 'themeTwo',
+    value: '2',
+    className: 'checked:bg-themeTwo-30 checked:hover:bg-themeTwo-40',
   },
   {
-    className: 'checked:bg-themeThree-30 checked:hover:bg-themeThree-40',
     id: 'theme-choice-three',
-    value: 'themeThree',
+    value: '3',
+    className: 'checked:bg-themeThree-30 checked:hover:bg-themeThree-40',
   },
 ];
 
@@ -31,12 +26,12 @@ const ThemeSwitcher = () => {
     <div class="ml-2 flex h-12 w-1/5 flex-col items-center">
       <form name="themeForm">
         <div class="flex h-6 w-16 items-center justify-between">
-          {Themes.map((theme) => (
-            <Label key={theme.id} value={theme.value} htmlFor={theme.id} />
+          {labelInput.map((label) => (
+            <Label key={label.id} value={label.value} htmlFor={label.id} />
           ))}
         </div>
         <div class="flex h-6 w-16 items-center justify-between rounded-lg bg-themeOne-20 ">
-          {inputProps.map((props, index) => (
+          {labelInput.map((props, index) => (
             <Input key={index} {...props} />
           ))}
         </div>
